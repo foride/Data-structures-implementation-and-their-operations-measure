aab60e44a35e95048235a95c607d29f64ddb63af
foride <faraminaed@wp.pl>
2023-03-29 14:54:21 +0200

    BinaryHeap implementation

---
+++ BinaryHeap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 1312 on 3/28/2023.
+//
+
+#include "BinaryHeap.h"
---
+++ BinaryHeap.h
@@ -0,0 +1,14 @@
+//
+// Created by 1312 on 3/28/2023.
+//
+
+#ifndef SDIZO_BINARYHEAP_H
+#define SDIZO_BINARYHEAP_H
+
+
+class BinaryHeap {
+
+};
+
+
+#endif //SDIZO_BINARYHEAP_H
